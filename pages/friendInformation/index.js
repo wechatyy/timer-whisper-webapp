3e4aeebf457133d49abb692fd6a71a8e11c4e8fa
王亚男 <wangyanan03@renrenche.com>
2020-01-12 19:59:43 +0800

    ..

--- pages/friendInformation/index.js
+++ pages/friendInformation/index.js
@@ -202,7 +202,13 @@ Page({
             success:()=>{
               _this.setData({
                 isOpens: false,
-                commVal:''
+
+                remarkname:this.data.commVal
+              })
+              _this.setData({
+
+                commVal:'',
+    
               })
             }
           })
